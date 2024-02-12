c331498d0ef7214f83a0335f0d432b21424a3878
mrmar99 <mrmar99@icloud.com>
2024-02-13 00:52:45 +0300

    edit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import path from 'path';
-import serveIndex from "serve-index";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
 app.use(express.json());
-app.use("/files", express.static("files"), serveIndex("files", { icons: true }));
+app.use("/files", express.static(path.join(__dirname, "files")));
 
 const port = 3001;
 app.listen(port, () => {
